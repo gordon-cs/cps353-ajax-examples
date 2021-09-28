7bc5c4e6edbc874bc953f85d1a82b893fc7451cf
Russ Tuck <russ.tuck@gordon.edu>
2021-09-28 16:13:41 -0400

    Update to current json structure

--- fetch.js
+++ fetch.js
@@ -7,8 +7,7 @@ async function fetchDemo() {
 
   console.log(JSON.stringify(myJson[0]));
   console.log(myJson[0].Event_Name, "starting at",
-              myJson[0].Occurrences[0][0]);
-  console.log(myJson[1].Event_Name, "in", myJson[0].Occurrences[0][2]);
+              myJson[0].Occurrences[0]["StartDate"]);
 }
 
 fetchDemo();
