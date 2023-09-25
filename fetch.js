10f707c35c2e2ee18183b57f6ac30f716d0179ab
Russ Tuck <russ.tuck@gordon.edu>
2023-09-25 11:17:24 -0400

    Fix comments, re-enable CORS

--- fetch.js
+++ fetch.js
@@ -2,13 +2,14 @@
 async function fetchDemo() {
   const response =
         await fetch("https://360api.gordon.edu/api/events/public",
-		    { mode: 'no-cors'})
+		   /* { mode: 'no-cors'} */ )
                 .then(response => console.log(response))
                 .catch(error => console.error(error));
   return;
+
+    /*
   const myJson = await response.json();
 
-  /*
   console.log(JSON.stringify(myJson[0]));
   console.log(myJson[0].Event_Name, "starting at",
               myJson[0].Occurrences[0]["StartDate"]);
