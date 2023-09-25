fe447480674c2f46e2911b002e8fd099920550fc
Russ Tuck <russ.tuck@gordon.edu>
2023-09-25 09:07:03 -0400

    clean up, but still CORS problems

--- fetch.js
+++ fetch.js
@@ -1,13 +1,18 @@
 
 async function fetchDemo() {
   const response =
-        await fetch("https://360api.gordon.edu/api/events/25Live/Public");
-
+        await fetch("https://360api.gordon.edu/api/events/public",
+		    { mode: 'no-cors'})
+                .then(response => console.log(response))
+                .catch(error => console.error(error));
+  return;
   const myJson = await response.json();
 
+  /*
   console.log(JSON.stringify(myJson[0]));
   console.log(myJson[0].Event_Name, "starting at",
               myJson[0].Occurrences[0]["StartDate"]);
+  */
 }
 
 fetchDemo();
